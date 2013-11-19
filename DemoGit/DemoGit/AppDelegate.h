bee305332c886b18d09980ca2a10c72b0439a82c
zuna <vh.trungkien@gmail.com>
2013-11-19 19:56:33 +0700

    a

---
+++ DemoGit/DemoGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoGit
+//
+//  Created by ios19 on 11/19/13.
+//  Copyright (c) 2013 ZunaCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoGit/DemoGit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoGit
+//
+//  Created by ios19 on 11/19/13.
+//  Copyright (c) 2013 ZunaCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
